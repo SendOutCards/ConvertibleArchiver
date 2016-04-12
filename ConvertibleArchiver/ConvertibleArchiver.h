7bb8957ca63b2b3ba267547354ba34014bb3a7a1
bradhilton <brad.hilton.nw@gmail.com>
2016-04-12 16:23:30 -0600

    Initial Commit

---
+++ ConvertibleArchiver/ConvertibleArchiver.h
@@ -0,0 +1,19 @@
+//
+//  ConvertibleArchiver.h
+//  ConvertibleArchiver
+//
+//  Created by Bradley Hilton on 4/12/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConvertibleArchiver.
+FOUNDATION_EXPORT double ConvertibleArchiverVersionNumber;
+
+//! Project version string for ConvertibleArchiver.
+FOUNDATION_EXPORT const unsigned char ConvertibleArchiverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvertibleArchiver/PublicHeader.h>
+
+
